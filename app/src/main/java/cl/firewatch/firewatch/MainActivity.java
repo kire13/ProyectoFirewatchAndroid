1df847d00e86f57672195b640c27e06777791b73
Freddy Huaylla Silvestre <hyliankz@gmail.com>
2018-11-07 18:21:13 -0300

    11/7/2018 EDITADO x Freddy  2

--- app/src/main/java/cl/firewatch/firewatch/MainActivity.java
+++ app/src/main/java/cl/firewatch/firewatch/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "x92", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "testupdate2", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "updatea2", Toast.LENGTH_SHORT).show();
     }
 }

54ecf94a161426f5a9d426849f2fe380b64483eb
kire <44845157+kire13@users.noreply.github.com>
2018-11-07 18:11:20 -0300

    KR_Update2_Test!

--- app/src/main/java/cl/firewatch/firewatch/MainActivity.java
+++ app/src/main/java/cl/firewatch/firewatch/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "x92", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "testupdate2", Toast.LENGTH_SHORT).show();
     }
 }

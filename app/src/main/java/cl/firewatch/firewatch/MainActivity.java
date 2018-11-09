0b2b8f834c12515f660c2d60bd604d835d5d77c3
kire <44845157+kire13@users.noreply.github.com>
2018-11-09 16:46:55 -0300

    update SwipeViews implements

--- app/src/main/java/cl/firewatch/firewatch/MainActivity.java
+++ app/src/main/java/cl/firewatch/firewatch/MainActivity.java
@@ -1,21 +1,26 @@
 package cl.firewatch.firewatch;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     ListView lv_dispositivos;
+    //Button bt_sincronizar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         lv_dispositivos=findViewById(R.id.lv_dispositivos);
+        //bt_sincronizar=findViewById(R.id.bt_sincronizar);
+
         String[] dispositivos={                             //cambiar a arralist de dispositivos
                 "Dispositivo1",
                 "Dispositivo2",
@@ -29,5 +34,10 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter=new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,dispositivos);
         lv_dispositivos.setAdapter(adapter);
 
+    }
+
+    public void Sicronizar(View view) {//Cambiar a HOME
+        Intent intent=new Intent(this, HomeActivity.class);
+        startActivity(intent);
     }
 }

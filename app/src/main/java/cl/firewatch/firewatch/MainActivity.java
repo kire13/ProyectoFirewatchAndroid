38df8be572000eee0cf24faaa54dfab12af82582
kire <44845157+kire13@users.noreply.github.com>
2018-11-11 01:09:01 -0300

    Test Update

--- app/src/main/java/cl/firewatch/firewatch/MainActivity.java
+++ app/src/main/java/cl/firewatch/firewatch/MainActivity.java
@@ -21,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
         lv_dispositivos=findViewById(R.id.lv_dispositivos);
         bt_sincronizar=findViewById(R.id.bt_sincronizar);
 
+        Toast.makeText(this, "test!!!",Toast.LENGTH_SHORT).show();
+
         String[] dispositivos={                             //cambiar a arralist de dispositivos !!!!!!!
                 "Dispositivo1",
                 "Dispositivo2",

2c3d586f4c2c87ae9fcebe824f157e84a9772611
kire <44845157+kire13@users.noreply.github.com>
2018-11-11 03:27:42 -0300

    KR Test PUSH X!

--- app/src/main/java/cl/firewatch/firewatch/MainActivity.java
+++ app/src/main/java/cl/firewatch/firewatch/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         lv_dispositivos=findViewById(R.id.lv_dispositivos);
         bt_sincronizar=findViewById(R.id.bt_sincronizar);
+        // PUSHHHHHHHHHHHHHHHHHHHHHHHHH"""""""""""""""!!!!
 
         Toast.makeText(this, "!!!!!!???????", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "WWWWWWWWWWWW", Toast.LENGTH_SHORT).show();

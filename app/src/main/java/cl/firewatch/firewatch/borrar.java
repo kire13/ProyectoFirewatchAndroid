5bde5e4d41935eba8889bd427d650d136c208ada
Freddy Huaylla Silvestre <hyliankz@gmail.com>
2018-11-11 02:43:07 -0300

    editado5

--- app/src/main/java/cl/firewatch/firewatch/borrar.java
+++ app/src/main/java/cl/firewatch/firewatch/borrar.java
@@ -10,6 +10,6 @@ public class borrar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_borrar);
-        Toast.makeText(this, "borrarrrrrrrrrrrrrr", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "borrarrrrrrrrrrrrrrrrr", Toast.LENGTH_SHORT).show();
     }
 }

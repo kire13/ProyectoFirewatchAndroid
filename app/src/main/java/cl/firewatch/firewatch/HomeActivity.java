4d37fb30721df97876e448cef3d143b75c74817e
Freddy Huaylla Silvestre <hyliankz@gmail.com>
2018-11-12 01:30:51 -0300

    editando pantalla principal 1

--- app/src/main/java/cl/firewatch/firewatch/HomeActivity.java
+++ app/src/main/java/cl/firewatch/firewatch/HomeActivity.java
@@ -3,10 +3,13 @@ package cl.firewatch.firewatch;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
-
+    //pantalla MAIN de 3 botones
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +19,26 @@ public class HomeActivity extends AppCompatActivity {
     public void Monitorizar(View view) {
         Intent intent = new Intent(this, SwipeActivity.class);
         startActivity(intent);
+    }
+
+    //genera menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_home_main, menu);
+        return super.onCreateOptionsMenu(menu);
+
+
+    }
+
+//  seleccionado
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        Intent Qintnto = new Intent(this, HomeActivity.class);
+        startActivity(Qintnto);
+        return super.onOptionsItemSelected(item);
+
     }
 }

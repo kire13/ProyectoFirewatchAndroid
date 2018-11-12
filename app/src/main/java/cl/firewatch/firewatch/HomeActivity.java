4d029d32b3e19f4d6c0bd0a0508359db460f014e
Freddy Huaylla Silvestre <hyliankz@gmail.com>
2018-11-12 04:38:44 -0300

    editando pantalla principal ,LISTO HOME,imagen firewatch ajustandoo, logo listo

--- app/src/main/java/cl/firewatch/firewatch/HomeActivity.java
+++ app/src/main/java/cl/firewatch/firewatch/HomeActivity.java
@@ -19,12 +19,10 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
 
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 //        String NombreActividad = this.getClass().getSimpleName();
         String DispSeleccionado = "";
-//sda
         getSupportActionBar().setTitle("Dispositivo :" + "#" + DispSeleccionado);
     }
 
@@ -66,13 +64,13 @@ public class HomeActivity extends AppCompatActivity {
 
     public void Controlar(View view) {
         Intent intent = new Intent(this, SwipeActivity.class);
-        intent.putExtra("pantalla",1);
+        intent.putExtra("pantalla", 1);
         startActivity(intent);
     }
 
     public void Seguimiento(View view) {
         Intent intent = new Intent(this, SwipeActivity.class);
-        intent.putExtra("pantalla",2);
+        intent.putExtra("pantalla", 2);
         startActivity(intent);
     }
 
